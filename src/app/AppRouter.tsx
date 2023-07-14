8b172224c50aa3b5c99947378ea210bf6f5822cd
deceser <aufhaummtobsc@gmail.com>
2023-07-14 23:27:39 +0300

    Set up basic react-route

---
+++ src/app/AppRouter.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+
+// some code should be added in the future
+
+const AppRouter = () => {
+  return (
+    <Routes>
+      <Route />
+    </Routes>
+  );
+};
+
+export default AppRouter;
---
+++ src/routes/index.ts
@@ -0,0 +1,7 @@
+export const publicRoutes = [
+  // some code should be added in the future
+];
+
+export const privatRoutes = [
+  // some code should be added in the future
+];
