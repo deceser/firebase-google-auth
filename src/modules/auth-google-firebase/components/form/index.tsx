1ebd78c8cfc2b86dd0d059b3b74a4468722d346e
deceser <aufhaummtobsc@gmail.com>
2023-07-19 16:01:34 +0300

    Created Interface IUser

--- src/modules/auth-google-firebase/components/form/index.tsx
+++ src/modules/auth-google-firebase/components/form/index.tsx
@@ -1,25 +1,19 @@
-import React, { MouseEvent, useState } from "react";
+import React from "react";
 import { googleProvider, auth } from "../../firabase";
 import style from "./index.module.scss";
-
-type FirebaseUser = {
-  uid: string;
-  displayName?: string;
-  email?: string;
-  photoURL?: string;
-};
+import { IUser } from "src/models/user";
 
 const AuthGoogleFirebase: React.FC = () => {
-  const [user, setUser] = useState<FirebaseUser | null>(null);
+  const [user, setUser] = React.useState<IUser | null>(null);
 
-  const handleGoogleLogin = async (event: MouseEvent<HTMLButtonElement>) => {
+  const handleGoogleLogin = async (event: React.MouseEvent<HTMLButtonElement>) => {
     event?.preventDefault();
     console.log("sdasdasd");
 
     try {
       const result = await auth.signInWithPopup(googleProvider);
-      const { uid, displayName, email, photoURL } = result.user as FirebaseUser;
-      const userData: FirebaseUser = {
+      const { uid, displayName, email, photoURL } = result.user as IUser;
+      const userData: IUser = {
         uid,
         displayName,
         email,
